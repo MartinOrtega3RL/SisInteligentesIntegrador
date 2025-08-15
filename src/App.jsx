aa572048c51298673137dab528a9835b8905c7fb
Martin Ortega <111008433+MartinOrtega3RL@users.noreply.github.com>
2025-08-15 12:52:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/foro" element={<Login />}
+          <Route path="/foro" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
